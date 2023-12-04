a65fb1fffd97799d859a4d4a4eda79574a8845da
sandrokg107 <sandrokg107@gmai.com>
2023-12-03 21:06:07 -0500

    Agregar archivos de proyecto.

---
+++ Lab12_AMM_Dominguez/Lab12_AMM_Dominguez/Models/TaskModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab12_AMM_Dominguez.Models
+{
+    public class TaskModel
+    {
+        public string Title { get; set; }
+        public bool Status { get; set; }
+        public string Description { get; set; }
+
+        public TaskModel(string title, bool status, string description)
+        {
+            Title = title;
+            Status = status;
+            Description = description;
+        }
+    }
+}
---
+++ Lab12_AMM_Dominguez/Lab12_AMM_Dominguez/ViewModel/TaskViewModel.cs
@@ -0,0 +1,87 @@
+﻿using Lab12_AMM_Dominguez.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lab12_AMM_Dominguez.ViewModel
+{
+    public class TaskViewModel : ViewModels.ViewModelBase
+    {
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private string description;
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (description != value)
+                {
+                    description = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private bool status;
+        public bool Status
+        {
+            get { return status; }
+            set
+            {
+
+                if (status != value)
+                {
+                    status = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private ObservableCollection<TaskModel> tasks;
+        public ObservableCollection<TaskModel> Tasks
+        {
+            get { return tasks; }
+            set
+            {
+                if (tasks != value)
+                {
+                    tasks = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand Save { protected set; get; }
+        public ICommand Get { protected set; get; }
+
+        public TaskViewModel()
+        {
+            ObservableCollection<TaskModel> TaskModels = new ObservableCollection<TaskModel>();
+            Save = new Command(() =>
+            {
+                TaskModels.Add(new TaskModel(Title, Status, Description));
+            });
+
+            Get = new Command(() =>
+            {
+                Tasks = new ObservableCollection<TaskModel>(TaskModels);
+            });
+        }
+    }
+}
